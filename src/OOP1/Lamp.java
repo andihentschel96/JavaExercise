eaf78366960be2aed337041a0770e71d96115c17
Andi Hentschel <andreas.hentschel@my-oli.com>
2023-07-11 15:16:55 +0200

    Test

---
+++ src/OOP1/Lamp.java
@@ -0,0 +1,27 @@
+package OOP1;
+
+public class Lamp {
+    //Fields
+    boolean isOn;
+
+    //methods
+    void turnOn() {
+        isOn = true;
+        System.out.println("Does it work? " + isOn);
+    }
+    void turnOff() {
+        isOn = false;
+        System.out.println("Does it work? " + isOn);
+    }
+
+    public static void main(String[] args) {
+        Lamp led = new Lamp();
+        Lamp flashlight = new Lamp();
+
+        led.turnOn();
+        flashlight.turnOn();
+
+        led.turnOff();
+        flashlight.turnOff();
+    }
+}
---
+++ src/OOP1/Methods/AllAboutMethods.java
@@ -0,0 +1,10 @@
+package OOP1.Methods;
+
+public class AllAboutMethods {
+    //Declaration of a Method:
+
+    //returnType methodName() {
+        //Code goes here
+    //}
+
+}

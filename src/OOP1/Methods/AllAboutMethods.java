3947956c8397d67c897096e4f9877d43531091e5
Andi Hentschel <andreas.hentschel@my-oli.com>
2023-07-11 16:05:12 +0200

    Test

--- src/OOP1/Methods/AllAboutMethods.java
+++ src/OOP1/Methods/AllAboutMethods.java
@@ -3,8 +3,24 @@ package OOP1.Methods;
 public class AllAboutMethods {
     //Declaration of a Method:
 
-    //returnType methodName() {
+    //returnType methodName(parameter1, parameter2,...) {
         //Code goes here
     //}
 
+    //Create a Method
+    public static int square(int i) {
+        //return-statement is always required with return types other than void
+        return i * i;
+    }
+
+
+    public static void main(String[] args) {
+        int result;
+        double result2;
+        // call the method
+        // store returned value to a variable (-> result)
+        result = square(12);
+
+        System.out.println("Squared value of 12 is " + result);
+    }
 }
